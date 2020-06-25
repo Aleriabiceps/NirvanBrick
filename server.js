3fa6098ca30b2bd7143b56bcdce8c19b682d985b
jleeson <jleeson2000@gmail.com>
2020-06-24 17:18:09 -0700

    fixed website description

--- server.js
+++
@@ -1,9 +0,0 @@
-const server = require("jolt-server");
-const path = require("path");
-
-server({
-    root: path.join(process.cwd(), "public"),
-    port: 80,
-    // key: "key file",
-    // cert: "cert file"
-});
